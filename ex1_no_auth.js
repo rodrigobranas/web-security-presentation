00e45b5e9138f7c80b3b4486b26ee71da250f2fa
Rodrigo Branas <rodrigo.branas@gmail.com>
2017-05-01 20:53:48 -0300

    updated

--- ex1_no_auth.js
+++ ex1_no_auth.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.send("OK");
+app.get('/books', function (req, res) {
+	res.json(["Clean Code", "Refactoring", "Extreme Programming"]);
 });
 
-app.listen(3000);
+app.listen(3001);
